3292d15ae292b288c42f50ee8961c549cc45cf28
olezhka0809 <olezhka.garnautan@gmail.com>
2024-04-15 17:09:52 +0300

    Hangman vrs 1.0

--- Hangman.c
+++ Hangman.c
@@ -1,50 +1,71 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
-
-int introduLitera( char cuvant[], int n, char litera, int count){
+void introduLitera( char cuvant[], char cuvantGasit[], int n, char litera ){
     int gasit =0;
     for (int i = 0; i < n; i++)
     {
-        if(cuvant[i] == litera){
-            printf("Litera este gasita la : %d \n", i+1);
-            printf("Numarul de incercari este %d\n", count);
-            count--;
+        if(cuvant[i] == litera || cuvant[i] == toupper(litera) || cuvant[i] == tolower(litera) ){
+            printf("Litera este gasita la pozitia: %d \n", i+1);
+            cuvantGasit[i] = cuvant[i];
             gasit++;
         }
     }
-    if(gasit !=0){
+
+    if(gasit ==0){
         printf("Litera nu a fost gasita !\n");
-        count--;
     }
-    return count;
-    
 }
 
 
 int main(){
+    printf("\n\tBine ai venit in jocul Hangman !!!\t\n");
     int n, count;
-    printf("Introdu numarul de  incercari : ");
-    scanf("%d",&count);
-    printf("Introdu numarul de caractere al cuvantului : ");
-    scanf("%d",&n);
-    char cuvant[n];
-     
-    printf("Introdu cuvantul: ");
-    scanf("%s", cuvant);
-    printf("Cuvantul introdus este %s \n", cuvant);
-    char litera;
 
-    printf("Numarul de incercari %d ", count);
-    while (count !=0)
-    {
-        printf("Introdu litera : \n");
-        scanf("%c", &litera);
-        printf("Litera alesa este %c ", litera);
-        count = introduLitera(cuvant, n, litera,count);
-        printf("count %d ",count);
-        printf("Mai ai %d incercari !",count);
+    char *cuvant;
+    char *cuvantGasit;
+
+    printf("Introdu cuvantul: ");
+
+    cuvant = (char*)malloc(100* sizeof(char));
+    scanf("%s", cuvant);
+    n = strlen(cuvant);
+
+    cuvantGasit= (char*)malloc((n+1)*sizeof(char));
+
+    printf("\n------ Cuvantul introdus este %s --------\n\n", cuvant);
+    char litera;
+    
+    for (int i = 0; i < n; i++) {
+        cuvantGasit[i] = '_';
     }
 
+    count= 2*n;
+    
+    cuvantGasit[n]='\0';
+    printf("Numarul de incercari %d ", count);
+    while (count >0)
+    {
+        printf("\nIntrodu litera :");
+        scanf(" %c", &litera);
+        count--;
+        introduLitera(cuvant, cuvantGasit, n,litera);
+        if(strcmp(cuvantGasit, cuvant)==0){
+            printf("\n------- Felicitari ai gasit cuvantul !!! ---------\n");
+            printf("Cuvantul : %s ", cuvant);
+            free(cuvant);
+            free(cuvantGasit);
+            return 0;
+        }
+        printf("Cuvantul tau arata astfel: \n");
+        printf("cuvant de ghicit :    %s   \n", cuvantGasit);
+        printf("Mai ai %d incercari !\n",count);
+    }
+
+    printf("\n--------- Ai pierdut !\nCuvantul corect era %s ---------\n", cuvant);
+    free(cuvant);
+    free(cuvantGasit);
     return 0;
 }
